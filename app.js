c7ab5276c2d5e3927d8e4ad500fc3d6769f86823
Zev Youra <zevyoura@gmail.com>
2014-12-28 17:48:47 -0800

    added picker, color legend

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ var data,       // raw csv data provided to d3.csv callback
     hourAxis,
     dayTicks,
     hourTicks,
+    colorLegend,
+    legendRect,
+    legendLabels,
     datumSelection,
     selectOptions,
-    colorScale, // d3 scales for colors
-    dayScale,
+    colorScale, // d3 scale for square coloring
+    dayScale,   // d3 ordinal scales for axis labels
     hourScale,
     width,      // dimensions (based on screen size)
     height,
-    chartWidth, 
-    chartHeight,
+    maxChartWidth, 
+    maxChartHeight,
     squareSize,
+    centerOffset,
+    chartWidth,
     metrics,    // array of possible metrics
     metric;     // metric being visualized, one of [total, alcoholrelated]
 
@@ -27,13 +32,26 @@ var SQUARE_PADDING = 5,
     HOURS_PER_DAY = 24,
     DAYS_PER_WEEK = 7,
     LEGEND_WIDTH = 40,
-    CHART_PADDING = 20,
+    CHART_PADDING = 10,
     DAY_LABELS = ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
     HOUR_LABELS = ['Midnight', '', '', '', '', '',
                        '6 AM', '', '', '', '', '',
                        'Noon', '', '', '', '', '',
                        '6 PM', '', '', '', '', ''];
-    METRIC_LABELS = [''];
+    METRIC_LABELS = {
+        'total': 'All incidents',
+        'alcoholrelated': 'Alcohol-related crashes',
+        'fatal': 'Fatal crashes',
+        'pedestrian': 'Crashes with pedestrians',
+        'bicycle': 'Bicycle crashes',
+        'motorcycle': 'Motorcycle crashes',
+        'sleep': 'Sleeping drivers',
+        'hitandrun': 'Hit and runs'
+    };
+
+var getTranslateString = function(x, y) {
+    return 'translate(' + x + ',' + y + ')';
+}
 
 var setMetric = function(metricToSet) {
     metric = metricToSet;
@@ -50,6 +68,9 @@ var setMetric = function(metricToSet) {
         }
     });
 
+    legendLabels.data([min, max]);
+    legendLabels.text(function(d) { return d; });
+
     colorScale
         .domain([min, max])
         .range([d3.rgb('white'), d3.rgb('green')]);
@@ -57,6 +78,9 @@ var setMetric = function(metricToSet) {
     datumSelection.transition().attr('fill', function(d,i) { 
         return colorScale(d[metric]); 
     });
+
+    // update selector
+    selectOptions.classed('selected', function(d) { return d == metric; });
 }
 
 var resize = function() {
@@ -67,23 +91,31 @@ var resize = function() {
         .attr('height', height);
 
     // calculate dimensions
-    chartWidth = width - LEGEND_WIDTH - (CHART_PADDING * 2);
-    chartHeight = height - LEGEND_WIDTH - (CHART_PADDING * 2);
-    squareSize = Math.floor(Math.min(100, 
-                                     chartWidth / (HOURS_PER_DAY + 1),
-                                     chartHeight / (DAYS_PER_WEEK + 1)
+    // 2 legends subtracted from width (day axis + color legend)
+    maxCaxChartWidth = width - (2 * LEGEND_WIDTH) - (CHART_PADDING * 2);
+    maxChartHeight = height - LEGEND_WIDTH - (CHART_PADDING * 2);
+    squareSize = Math.floor(Math.min(50, 
+                                     maxCaxChartWidth / (HOURS_PER_DAY + 1),
+                                     maxChartHeight / (DAYS_PER_WEEK + 1)
                             )) - SQUARE_PADDING;
+    chartWidth = HOURS_PER_DAY * (squareSize + SQUARE_PADDING);
+    centerOffset = (width - chartWidth - 2 * LEGEND_WIDTH) / 2; 
 
     // update axis positions
-    dayTicks.attr('y', function(d, i) { return CHART_PADDING + LEGEND_WIDTH + ((i + 0.5) * (squareSize + SQUARE_PADDING))})
-    hourTicks.attr('x', function(d, i) { return CHART_PADDING + LEGEND_WIDTH + (i * (squareSize + SQUARE_PADDING))})
+    dayTicks
+        .attr('x', CHART_PADDING + centerOffset)
+        .attr('y', function(d, i) { return CHART_PADDING + LEGEND_WIDTH + ((i + 0.5) * (squareSize + SQUARE_PADDING))});
+    hourTicks.attr('x', function(d, i) { return centerOffset + CHART_PADDING + LEGEND_WIDTH + (i * (squareSize + SQUARE_PADDING))});
+    colorLegend.attr('transform', getTranslateString((CHART_PADDING * 2) + centerOffset + LEGEND_WIDTH + chartWidth, CHART_PADDING + LEGEND_WIDTH));
+    legendRect.attr('height', (squareSize + SQUARE_PADDING) * DAYS_PER_WEEK - SQUARE_PADDING);
+    legendLabels.attr('y', function(d, i) { return i == 0 ? 18 : (squareSize + SQUARE_PADDING) * DAYS_PER_WEEK - SQUARE_PADDING; });
 
     // update chart data positions
     datumSelection
         .attr('width', squareSize)
         .attr('height', squareSize)
         .attr('x', function(d,i) { 
-            return CHART_PADDING + LEGEND_WIDTH + d.hour * (squareSize + SQUARE_PADDING); 
+            return centerOffset + CHART_PADDING + LEGEND_WIDTH + d.hour * (squareSize + SQUARE_PADDING); 
         }).attr('y', function(d,i) {
             // day of week is 1 indexed per data source
             return CHART_PADDING + LEGEND_WIDTH + (d.dayweek - 1) * (squareSize + SQUARE_PADDING);
@@ -103,26 +135,27 @@ var processData = function(csv) {
 
     metrics = d3.map(data[0]).keys().filter(function(key) { return key != 'dayweek' && key != 'hour'; });
 
-    selectElt = d3.select('select');
-    selectElt.selectAll('option').remove(); // clear loading option
-    selectOptions = selectElt.selectAll('option').data(metrics)
-        .enter().append('option')
-            .text(function(d,i) { return d; });
-    selectElt.on('change', function() {
-        setMetric(selectOptions[0][selectElt.property('selectedIndex')].text);
-    });
+    selectElt = d3.select('.selector')
+        .text('')
+        .on('mouseover', function(d, i) { selectElt.classed('open', true); })
+        .on('mouseout', function(d, u) { selectElt.classed('open', false); });
+    selectOptions = selectElt.selectAll('.option').data(metrics)
+        .enter().append('div')
+            .classed('option', true)
+            .text(function(d,i) { return METRIC_LABELS[d]; })
+            .on('click', function(d) {
+                selectElt.classed('open', false);
+                setMetric(d);
+            });
 }
 
-// load csv formated data; originally sourced from http://tims.berkeley.edu/page.php?page=switrs_resources
-d3.csv('data.csv', function(d) {
-    processData(d);
-
+var setupChart = function() {
     // remove loader
     d3.select('.loading').remove();
 
     // set up basic d3 selections
     host = d3.select('.content');
-    svg = host.append('svg');
+    svg = host.select('svg');
 
     // set up scales
     dayScale = d3.scale.ordinal()
@@ -139,7 +172,6 @@ d3.csv('data.csv', function(d) {
         .classed('day', true);
     dayTicks = dayAxis.selectAll('.tick').data(d3.range(1, 8))
         .enter().append('text')
-            .attr('x', CHART_PADDING)
             .classed('tick', true)
             .text(function(d) { return dayScale(d); });
     hourAxis = svg.append('g')
@@ -150,10 +182,25 @@ d3.csv('data.csv', function(d) {
             .attr('y', CHART_PADDING + (LEGEND_WIDTH / 2))
             .classed('tick', true)
             .text(function(d) { return hourScale(d); });
+    colorLegend = svg.append('g')
+        .classed('legend', true);
+    legendRect = colorLegend.append('rect')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', LEGEND_WIDTH / 2);
+    legendLabels = colorLegend.selectAll('text').data([0,1]).enter().append('text')
+        .attr('x', LEGEND_WIDTH / 2 + 4);
 
     // make basic 24x7 chart
-    datumSelection = svg.selectAll('.datum').data(d)
+    datumSelection = svg.selectAll('.datum').data(data)
         .enter().append('rect');
+}
+
+// load csv formated data; originally sourced from http://tims.berkeley.edu/page.php?page=switrs_resources
+d3.csv('data.csv', function(d) {
+    processData(d);
+
+    setupChart(d);
 
     resize();
     window.addEventListener('resize', resize);
